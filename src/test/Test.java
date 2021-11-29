d3bac841a2be9c32722ceafcb5ba0d0b23336be4
InitialShip <motosawachii@gmail.com>
2021-11-30 01:16:09 +0700

    initial commit

--- src/test/Test.java
+++ src/test/Test.java
@@ -1,5 +1,11 @@
 package test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class Test {
-    
+    @org.junit.jupiter.api.Test
+    public void testfunction()
+    {
+        assertEquals(1, 2);
+    }
 }
