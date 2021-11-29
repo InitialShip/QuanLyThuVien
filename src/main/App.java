e995acdef49f83c7e66e488a73b034a1800ce222
InitialShip <motosawachii@gmail.com>
2021-11-30 00:47:58 +0700

    Initial commit

---
+++ src/main/App.java
@@ -0,0 +1,26 @@
+package main;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application{
+    
+    @Override
+    public void start(Stage stage) throws IOException {
+        Scene scene = new Scene(loadFXML("scene/primary"));
+        stage.setScene(scene);
+        stage.show();
+    }
+    private static Parent loadFXML(String fxml) throws IOException{
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource(fxml+".fxml"));
+        return fxmlLoader.load();
+    }
+    public static void main(String[] args) throws Exception {
+        launch();
+    }
+
+}
---
+++ src/main/scene/PrimaryController.java
@@ -0,0 +1,10 @@
+package main.scene;
+
+import javafx.fxml.FXML;
+
+public class PrimaryController {
+    @FXML
+    private void sayHello(){
+        System.out.println("Hello World!");
+    }
+}
