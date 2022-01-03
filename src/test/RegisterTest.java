d9294bd477ce272d971d6636e300296116d2b6c9
InitialShip <motosawachii@gmail.com>
2022-01-03 20:49:56 +0700

    update

--- src/test/RegisterTest.java
+++ src/test/RegisterTest.java
@@ -2,10 +2,15 @@ package test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+import java.security.NoSuchAlgorithmException;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 import org.junit.Test;
 
+import main.mySqlConnector.Connector;
+import main.service.LoginService;
 import main.service.RegisterService;
 
 public class RegisterTest {
@@ -18,5 +23,21 @@ public class RegisterTest {
         assertEquals(RegisterService.isFromSchool("EP065106708"), false);
         assertEquals(RegisterService.isFromSchool("CS06510678"), false);
     }
-    
+    //CS1960068 Abc12345
+    @Test
+    public void testRegister() throws NoSuchAlgorithmException, SQLException{
+        RegisterService.register("CS1960068", "Abc12345");
+        assertEquals(LoginService.isAccountExisted("CS1960068"), true);
+        assertEquals(LoginService.isPasswordCorrect("CS1960068", "Abc12345"), true);
+        assertEquals(LoginService.isPasswordCorrect("CS1960068", "aBc12345"), false);
+        RegisterService.createIdCard("CS1960068");
+        Connector.open();
+        Statement statement = Connector.getCnt().createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM library_db.id_card WHERE user_id = 'CS1960068'");
+        while(rs.next()){
+            assertEquals(rs.getString(1), "CS1960068");
+        }
+        statement.close();
+        Connector.close();
+    }
 }
